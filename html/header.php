8edd41006469ff12bdd7b3f9381950c349cd2ae3
paul69800 <47972711+paul69800@users.noreply.github.com>
2019-02-25 11:52:00 +0100

    Update header.php

--- html/header.php
+++ html/header.php
@@ -1,7 +1,8 @@
 <!-- entête de page
-auteur:Thibaud Jacquelin
+auteur:Thibaud
 09.02.2019
 -->
+
 <header>
 <a href="index.php" title="Revenir à l'accueil"><h1 class=nomdusite>Wikipuncture</h1></a>
 </header>
